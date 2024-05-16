c9a2df4355ba2f4d8116d9bcd5604662ed545ccb
Aizawa <130658362+NikeshDePhoenix@users.noreply.github.com>
2024-05-16 16:29:19 +0545

    Coffee.py

---
+++ Coffee.py
@@ -0,0 +1,16 @@
+print("Welcome to karens Coffee shop,\n What is you good name sir ??")
+
+bhaude = input()
+
+print("Thank you so much for coming " + bhaude)
+
+sugay = "Black coffee,Latte,Espreso,Cappucino"
+
+print("What could you like today from our Menu, \n"  + sugay)
+
+rajan = input()
+
+print("Dear " + bhaude , " Your " + rajan, "Will be seved in a minute or so")
+
+print("Thanks for comming in")
+
